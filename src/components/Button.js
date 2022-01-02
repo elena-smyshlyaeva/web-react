53d3eeb5072bd84722ab2e52c71322617cd19940
Elena <elena.smyshlyaeva01@gmail.com>
2022-01-02 21:14:36 +0300

    фикс кнопки

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,29 +1,14 @@
 import React from "react";
 import './Button.css'
-import {Link} from 'react-router-dom';
-
-const STYLES = ['btn--primary', 'btn--outline'];
-
-const SIZES = ['btn--medium', 'btn--large'];
 
 export const Button = ({
-                           children,
-                           type,
-                           onClick,
-                           buttonStyle,
-                           buttonSize
+                           children, //аргумент внутри тега
+                           onClick //функция, реагирующая на клик
 }) => {
-    const checkButtonStyle = STYLES.includes(buttonStyle)
-        ? buttonStyle
-        : STYLES[0];
-
-    const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
-
     return(
         <button
-            className={`btn ${checkButtonStyle} ${checkButtonSize}`}
             onClick={onClick}
-            type={type}
+            className={`btn btn--outline btn--medium`}
         >
             {children}
         </button>
