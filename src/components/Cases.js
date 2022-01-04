02966106d66a2371f12e9b34fa7e739080d5879d
Elena <elena.smyshlyaeva01@gmail.com>
2022-01-04 16:40:22 +0300

    секция кейсов

---
+++ src/components/Cases.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Cases.css';
+
+function Cases() {
+    return (
+        <div className="cases">
+            <div className="cases-container">
+                <div className="cases-title">
+                    Последние кейсы
+                </div>
+
+            </div>
+        </div>
+    );
+}
+
+export default Cases;
