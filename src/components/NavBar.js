d3b6d2d6e82ffef124cef2710b2ed6e36b99d038
Elena <elena.smyshlyaeva01@gmail.com>
2022-01-05 14:00:33 +0300

    сделан роутинг

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import { ReactComponent as LogoSvg} from "../components/images/logo.svg";
 import './NavBar.css';
-import {Link} from "react-router-dom";
+import { Link } from 'react-scroll';
 
 function NavBar() {
     //состояние нажатия на элемент изменяется функцией setClick
@@ -46,7 +46,7 @@ function NavBar() {
             <nav className="navbar" id={navbarClasses.join('-')}>
                 <div className='navbar-container'>
                     <div className="navbar-logo-menu">
-                        <Link to='Home' className='navbar-logo' onClick={closeMobileMenu}>
+                        <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
                             <LogoSvg/>
                         </Link>
                         <div className="menu-icon" onClick={handleClick}>
@@ -55,40 +55,79 @@ function NavBar() {
 
                         <ul className={click ? 'nav-menu active' : 'nav-menu'}>
                             <li className='nav-item'>
-                                <Link to='/support' className='nav-links' onClick={closeMobileMenu} spy={true} offset={10}
-                                      activeClass="active"
+                                <Link
+                                    to='support' /*id компонента, куда ведет ссылка*/
+                                    className='nav-links'
+                                    onClick={closeMobileMenu}
+                                    spy={true} /*позволяет оставлять ссылку выбранной, когда скролл на нужной секции*/
+                                    activeClass="active" /*название класса, который добавляется, когда на нужной секции*/
                                 >
                                     ПОДДЕРЖКА САЙТОВ
                                 </Link>
+                                <div className="dropdown-button-menu"/>
+                                <ul className="nav-dropdown-list">
+                                    <li className="nav-dropdown-item">
+                                        МИГРАЦИЯ
+                                    </li>
+                                    <li className="nav-dropdown-item">
+                                        БЭКАПЫ
+                                    </li>
+                                    <li className="nav-dropdown-item">
+                                        АУДИТ БЕЗОПАСНОСТИ
+                                    </li>
+                                    <li className="nav-dropdown-item">
+                                        ОПТИМИЗАЦИЯ СКОРОСТИ
+                                    </li>
+                                    <li className="nav-dropdown-item">
+                                        ПЕРЕЕЗД НА HTTPS
+                                    </li>
+                                </ul>
                             </li>
 
                             <li className='nav-item'>
-                                <Link to='/rates' className='nav-links' onClick={closeMobileMenu} spy={true} offset={-100}
-                                      activeClass="active"
+                                <Link
+                                    to='plans'
+                                    className='nav-links'
+                                    onClick={closeMobileMenu}
+                                    spy={true}
+                                    activeClass="active"
                                 >
                                     ТАРИФЫ
                                 </Link>
                             </li>
 
                             <li className='nav-item'>
-                                <Link to='/cases' className='nav-links' onClick={closeMobileMenu} spy={true} offset={-70}
-                                      activeClass="active"
+                                <Link
+                                    to='cases'
+                                    className='nav-links'
+                                    onClick={closeMobileMenu}
+                                    spy={true}
+                                    offset={-100}
+                                    activeClass="active"
                                 >
                                     НАШИ РАБОТЫ
                                 </Link>
                             </li>
 
                             <li className='nav-item'>
-                                <Link to='/reviews' className='nav-links' onClick={closeMobileMenu} spy={true} offset={50}
-                                      activeClass="active"
+                                <Link
+                                    to='reviews'
+                                    className='nav-links'
+                                    onClick={closeMobileMenu}
+                                    spy={true}
+                                    activeClass="active"
                                 >
                                     ОТЗЫВЫ
                                 </Link>
                             </li>
 
                             <li className='nav-item'>
-                                <Link to='/contacts' className='nav-links' onClick={closeMobileMenu} spy={true} offset={0}
-                                      activeClass="active"
+                                <Link
+                                    to='contacts'
+                                    className='nav-links'
+                                    onClick={closeMobileMenu}
+                                    spy={true}
+                                    activeClass="active"
                                 >
                                     КОНТАКТЫ
                                 </Link>
