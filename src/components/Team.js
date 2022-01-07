0f8ed28a56bbea8476b05c3f008b858b617925fb
devilmah <91089885+devilmah@users.noreply.github.com>
2022-01-07 23:56:38 +0300

    Update Team.js

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import './Team.css';
-import { ReactComponent as Member1 } from "../components/images/image 2.svg";
-import { ReactComponent as Member2 } from "../components/images/image 6.svg";
-import { ReactComponent as Member3 } from "../components/images/image 4.svg";
-import { ReactComponent as Member4 } from "../components/images/image 4.svg";
-import { ReactComponent as Member5 } from "../components/images/image 2.svg";
-import { ReactComponent as Member6 } from "../components/images/image 6.svg";
-
+import Member1 from "../components/images/image2.svg";
+import Member2 from "../components/images/image6.svg";
+import Member3 from "../components/images/image4.svg";
+import Member4 from "../components/images/image4.svg";
+import Member5 from "../components/images/image2.svg";
+import Member6 from "../components/images/image6.svg";
 
 function Team() {
     return (
@@ -15,33 +14,33 @@ function Team() {
                 <h1 className="team-title">Команда</h1>
                 <div className="team-items">
                     <div className="team-item">
-                        <Member1 />
+                        <img src={Member1} alt="img" className="team-item-img"/>
                         <div className="team-item-title">Лёша</div>
                         <div className="team-item-desc">руководитель поддержки, планирование задач</div>
                     </div>
                     <div className="team-item">
-                        <Member2 />
+                        <img src={Member2} alt="img" className="team-item-img" />
                         <div className="team-item-title">Роман</div>
                         <div className="team-item-desc">инфраструктура веб-проектов</div>
                     </div>
                     <div className="team-item">
-                        <Member2 />
+                        <img src={Member3} alt="img" className="team-item-img" />
                         <div className="team-item-title">Ирина</div>
                         <div className="team-item-desc">менеджер по работе
                                      с клинетами, организация оказания услуг</div>
                     </div>
                     <div className="team-item">
-                        <Member2 />
+                        <img src={Member4} alt="img" className="team-item-img" />
                         <div className="team-item-title">Даша</div>
                         <div className="team-item-desc">SEO, веб-маркетинг</div>
                     </div>
                     <div className="team-item">
-                        <Member2 />
+                        <img src={Member5} alt="img" className="team-item-img" />
                         <div className="team-item-title">Сергей</div>
                         <div className="team-item-desc">технический директор, 14 лет опыт работы с Drupal</div>
                     </div>
                     <div className="team-item">
-                        <Member2 />
+                        <img src={Member6} alt="img" className="team-item-img" />
                         <div className="team-item-title">Вадим</div>
                         <div className="team-item-desc">UX/UI дизайн</div>
                     </div>
