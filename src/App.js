e135bb38fdc7099bd4d643a1f84be7c71eec20e9
devilmah <91089885+devilmah@users.noreply.github.com>
2022-01-08 08:48:40 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import React from 'react';
 import './App.css';
 import Team from './components/Team';
+import Rewiews from './components/Rewiews';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
       <>
-              <Team/>
+          <Team/>
+          <Rewiews/>
       </>
   );
 }
