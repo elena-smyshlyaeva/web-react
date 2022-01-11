155f1c313466d9898915adf16247f96856030a1f
devilmah <91089885+devilmah@users.noreply.github.com>
2022-01-11 03:27:03 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Team from './components/Team';
 import Rewiews from './components/Rewiews';
 import Webform from './components/Webform';
+import Plans from './components/Plans';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
       <>
+          <Plans/>
           <Team/>
           <Rewiews/>
           <Webform/>
