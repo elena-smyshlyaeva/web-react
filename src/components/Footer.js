e9018fee7d234f91ce0c385ecfe1e55a070feb4e
devilmah <91089885+devilmah@users.noreply.github.com>
2022-01-11 04:16:42 +0300

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+        <footer className="footer">
+                <div className="footer-container">
+                    <div className="footer-text">
+                        Проект ООО «Инитлаб», Краснодар, Россия. <br /> Drupal является
+                        зарегистрированной торговой маркой Dries Buytaert.
+                    </div>
+                </div>
+        </footer>
+    );
+}
+
+export default Footer;
